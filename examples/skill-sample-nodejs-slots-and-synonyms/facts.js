86c699b9d147b6c04bff7f5d29d790a88fed2e88
Emma Maloney <emma.maloney@bbc.co.uk>
2017-08-08 12:38:44 +0100

    Add example tests for slots and resolutions

---
+++ examples/skill-sample-nodejs-slots-and-synonyms/facts.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'new york': [
+    'New Yorkers drink 7 times more coffee than people in the rest of America',
+    'Roughly half of the population speaks a language other than English at home',
+    'In 1789 New York was the first capital of the United States. This only lasted for one year though',
+    '15,152 forms of life have been found in the New York Subway system (these include insects and bacteria'
+  ],
+  'las vegas': [
+    'More than 41 million people visit Las Vegas each year',
+    'Over 22,000 conventions are held in Las Vegas every year',
+    'There are more than 150 casinos in the Las Vegas valley area'
+  ],
+  'los angeles': [
+    'The Hollywood sign originally said Hollywood land',
+    'It is illegal to lick a toad in the City of Angels',
+    'Beverly Hills started out as a modest lima bean ranch'
+  ],
+  'anchorage': [
+    'The city of Anchorage got its start in 1914 when it was chosen at the site of a railroad-construction port for the Alaska Railroad',
+    'With a population around 300,000, Anchorage is Alaska’s largest city',
+    'Approximately 250 black bears and 60 grizzly bears live within urban Anchorage and the surrounding area'
+  ],
+  'honolulu': [
+    'Within the city limits of Honolulu, it is illegal to annoy a bird',
+    'Honolulu has been named the third happiest city to work in by Forbes magazine',
+    'The stunning Cathedral of Our Lady Peace in Honolulu was built in 1943 and it\'s the oldest Catholic cathedral used continuously in all of the United States'
+  ],
+  'chicago': [
+    'The atom was first split, leading to the A bomb and nuclear power, under the football stands of Stagg Field at the University of Chicago',
+    'The term Jazz was coined in Chicago in 1914',
+    'Over 52 million people visit Chicago annually'
+  ]
+};
